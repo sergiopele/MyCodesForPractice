7fdad9d4f1785f5aa232c1ee11e933b3e4ba5f05
sergiopele <sergiypele@gmail.com>
2023-01-03 03:52:15 -0500

    "Practical part"

--- src/Solution.java
+++ src/Solution.java
@@ -23,37 +23,7 @@ public class Solution {
 		//.forEach(System.out::println);
 		//System.out.println(lastChars("last","chars"));
 		
-		System.out.println(detectCapitalUse("FlaG"));
-	}
-	
-	class Solution {
-		public boolean wordPattern(String pattern, String s) {
-			String [] S = s.split("\\s+");
-			char [] C = pattern.toCharArray();
-			
-			Set<String> stringSet = new HashSet<>();
-			Set<Character> charSet = new HashSet<>();
-			for(String sArr : S){
-				stringSet.add(sArr);
-			}
-			for(Character cArr : C){
-				charSet.add(cArr);
-			}
-			if(stringSet.size()!=charSet.size()) return false;
-			
-			Map <Character, String> map = new HashMap<>();
-			for(int i = 0; i < stringSet.size(); i++){
-				map.put(charSet., stringSet.get(i));
-			}
-			for(int i = 0; i < S.length; i++){
-				
-				if(!map.get(C[i]).equals(S[i])){
-					return false;
-				}
-			}
-			return true;
-			
-		}
+		
 	}
 }
 	
